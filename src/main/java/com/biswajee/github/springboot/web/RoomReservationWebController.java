fa5b8146bce85e502461ab03f2b9ed3b5c5332d9
Biswajee <roy.biswajeet161@gmail.com>
2020-07-06 21:00:00 +0530

    Create package web

---
+++ src/main/java/com/biswajee/github/springboot/web/RoomReservationWebController.java
@@ -0,0 +1,13 @@
+package com.biswajee.github.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class RoomReservationWebController {
+
+	@Controller
+	@RequestMapping("/reservations")
+	public class RoomReservationWebController {
+		
+	}
+}
